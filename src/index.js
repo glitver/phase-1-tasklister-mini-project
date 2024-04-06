e37adcc2ce567f9815fed5e60d9b6486e2911da7
+ <+>
2024-04-06 17:50:18 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let taskInput = document.getElementById('new-task-description');
+    let taskDescription = taskInput.value;
+    let taskList = document.getElementById('tasks');
+    let newTaskItem = document.createElement('li');
+    newTaskItem.textContent = taskDescription;
+    taskList.appendChild(newTaskItem);
+    taskInput.value = '';
+  })
 });
+
+let deleteButton = document.createElement('button')
+deleteButton.textContent = 'x';
+deleteButton.addEventListener('click', function () {
+  newTaskItem.remove();
+
+newTaskItem.appendChild(deleteButton);
+taskList.appendChild(newTaskItem);
+
+taskDescriptionInput.value = '';
